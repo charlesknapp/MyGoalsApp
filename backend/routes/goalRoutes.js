92f1a9be56f283f2eaff19bba73d7d6fb538e90b
charlesK <115595915+charlesknapp@users.noreply.github.com>
2022-11-21 15:29:48 -0700

    Goals controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const { getGoal, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+
+// CRUD ROUTES
+
+router.route('/').get(getGoal).post(setGoal);
+router.route('/:id').delete(deleteGoal).put(updateGoal);
+// router.get("/", getGoal);
+// router.post("/", (setGoal));
+// router.put("/:id", (updateGoal));
+// router.delete("/:id", (deleteGoal));
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT;
+
+const app = express();
+
+// middleware
+app.use (express.json());
+app.use(express.urlencoded({extended: false}))
+
+// goal routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+// overwriting the default express error handler
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
